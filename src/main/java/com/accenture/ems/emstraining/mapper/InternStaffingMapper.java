636787e98697b8ae9c0008689cd6d9aaa008591a
Mikus Rakis <mikus.rakis@gmail.com>
2026-09-16 19:04:40 +0300

    Add staffing mapper

---
+++ src/main/java/com/accenture/ems/emstraining/mapper/InternStaffingMapper.java
@@ -0,0 +1,22 @@
+package com.accenture.ems.emstraining.mapper;
+
+import com.accenture.ems.emstraining.entity.InternStaffing;
+import com.accenture.ems.emstraining.model.InternStaffingResponse;
+import com.accenture.ems.emstraining.model.InternStaffingSummaryResponse;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface InternStaffingMapper {
+
+    List<InternStaffingSummaryResponse> toResponseList(List<InternStaffing> staffings);
+
+    @Mapping(source = "employee.employeeId", target = "employeeId")
+    @Mapping(source = "employee.surname", target = "employeeSurname")
+    @Mapping(source = "internHiringStatus.status", target = "internHiringStatus")
+    InternStaffingSummaryResponse toSummaryResponse(InternStaffing staffing);
+
+    InternStaffingResponse toResponse(InternStaffing staffing);
+}
